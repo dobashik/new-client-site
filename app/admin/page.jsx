5283228a80cfc1a8359dd60f6cceb4470cc0b55d
dobashi.kentarou <dobakenjp@gmail.com>
2025-07-30 09:17:19 +0900

    Add admin page and CMS config

---
+++ app/admin/page.jsx
@@ -0,0 +1,9 @@
+export default function Admin() {
+  return (
+    <iframe
+      src="/admin/index.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+      title="Decap CMS Admin"
+    />
+  );
+}
